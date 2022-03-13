b18009325de6a23bd65ec58518235ad7598df8b6
Aman Kalra <aman.kalra4@gmail.com>
2022-03-14 01:27:01 +0530

    Resolving build error

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,21 +1,21 @@
 import { listContainer } from "./style";
-import { navItems } from "./constants";
+// import { navItems } from "./constants";
 
 type navBarType = {
-  display: boolean;
-  onClick: () => void;
+    display: boolean;
+    onClick: () => void;
 };
 
 const NavBar = ({ display, onClick }: navBarType) => {
-  return (
-    <div className={listContainer}>
-      {navItems.map((el, index) => (
+    return (
+        <div className={listContainer}>
+            {/* {navItems.map((el, index) => (
         <li key={String(index + 1)} style={{ paddingBottom: "2rem" }}>
           {el}
         </li>
-      ))}
-    </div>
-  );
+      ))} */}
+        </div>
+    );
 };
 
 export default NavBar;
